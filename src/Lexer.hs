c22340d357437b12c491417d98e53aaf5104e771
youkwhd <lolywk@tutanota.com>
2023-11-08 19:04:04 +0700

    chore: tidy up

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -8,18 +8,20 @@ data Token = Token {
     _type :: TokenType
 }
 
-data TokenType =
-    UNKNOWN    |
-    WHITESPACE |
-    FUNC       |
-    LPAREN     |
-    RPAREN     |
-    NUMBER     |
-    NEWLINE    |
-    PLUS       |
-    MINUS      |
-    TIMES      |
-    DIVIDE deriving (Eq, Ord, Enum, Show)
+data TokenType
+    = UNKNOWN
+    | EOF
+    | WHITESPACE
+    | FUNC
+    | LPAREN
+    | RPAREN
+    | NUMBER
+    | NEWLINE
+    | PLUS
+    | MINUS
+    | TIMES
+    | DIVIDE
+    deriving (Eq, Ord, Enum, Show)
 
 consumeWith :: String -> (String -> Bool) -> (String, String)
 consumeWith "" _ = ("", "")
@@ -36,18 +38,22 @@ readNumber str =
     let result = fst consumed in
     (Token {literal = result, _type = if result == "" then UNKNOWN else NUMBER}, snd consumed)
 
+nextToken :: String -> (Token, String)
+nextToken "" = (Token {literal = "", _type = UNKNOWN}, "")
+nextToken (ch:str) = case ch of
+    ' ' -> (Token {literal = " ", _type = WHITESPACE}, str)
+    '\t' -> (Token {literal = "\t", _type = WHITESPACE}, str)
+    '(' -> (Token {literal = "(", _type = LPAREN}, str)
+    ')' -> (Token {literal = ")", _type = RPAREN}, str)
+    '+' -> (Token {literal = "+", _type = PLUS}, str)
+    '-' -> (Token {literal = "-", _type = MINUS}, str)
+    '*' -> (Token {literal = "*", _type = TIMES}, str)
+    '/' -> (Token {literal = "/", _type = DIVIDE}, str)
+    ch | isDigit ch -> readNumber (ch : str)
+    ch -> (Token {literal = ch : "", _type = UNKNOWN}, str)
+
 tokenize :: String -> [Token]
 tokenize "" = []
-tokenize (ch:str) = case ch of
-    ' ' -> Token {literal = " ", _type = WHITESPACE} : tokenize str
-    '\t' -> Token {literal = "\t", _type = WHITESPACE} : tokenize str
-    '(' -> Token {literal = "(", _type = LPAREN} : tokenize str
-    ')' -> Token {literal = ")", _type = RPAREN} : tokenize str
-    '+' -> Token {literal = "+", _type = PLUS} : tokenize str
-    '-' -> Token {literal = "-", _type = MINUS} : tokenize str
-    '*' -> Token {literal = "*", _type = TIMES} : tokenize str
-    '/' -> Token {literal = "/", _type = DIVIDE} : tokenize str
-    ch | isDigit ch ->
-        let consumed = readNumber (ch : str) in
-        fst consumed : tokenize (snd consumed)
-    ch -> Token {literal = ch : "", _type = UNKNOWN} : tokenize str
+tokenize str = 
+    let result = nextToken str in
+    fst result : tokenize (snd result)
