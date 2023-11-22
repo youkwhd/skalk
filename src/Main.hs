3f6a00c8d0bb4fcdecb344b88d6f4574ce9a7966
youkwhd <lolywk@tutanota.com>
2023-11-23 00:51:51 +0700

    chore: abort making it compiled

--- src/Main.hs
+++ src/Main.hs
@@ -6,27 +6,5 @@ import Control.Monad (forM)
 
 main :: IO ()
 main = do
-    let prog = "(+ 6 2)" 
-    let asm = "section .data\n\
-              \    fmt: db \"%d\", 0x0A, 0x0D, 0x00\n\
-              \\n\
-              \section .text\n\
-              \    global main\n\
-              \    extern printf\n\
-              \\n\
-              \exit:\n\
-              \    mov rax, 0x3C\n\
-              \    mov rdi, 0x00\n\
-              \    syscall\n\
-              \    ret\n\
-              \\n\
-              \main:\n"
-              ++
-              parse (tokenize prog)
-              ++
-              "\n    mov rdi, fmt\n\
-              \    mov rsi, rax\n\
-              \    mov eax, 0x00\n\
-              \    call printf\n\
-              \    call exit\n" 
-    putStr asm
+    let prog = "(+ 5 (* 2 4) (+ 1 1))" 
+    print (parse (tokenize prog))
--- src/Parser.hs
+++ src/Parser.hs
@@ -3,35 +3,39 @@ module Parser where
 import Lexer
 import Control.Exception
 
+parse :: [Token] -> Int
+parse tokens =  __parse tokens [] [] []
+
 peek :: [a] -> Maybe a
 peek [] = Nothing
 peek (x:xs) = Just x
 
-parse :: [Token] -> String
-parse tokens =  __parse tokens []
-
--- [TokenType] stack of token types
-__parse :: [Token] -> [TokenType] -> String
-__parse [] operations = ""
-__parse (tok:tokens) operations =
+__parse :: [Token] -> [(TokenType, Int)] -> [TokenType] -> [Int] -> Int
+__parse [] _ _ numbers = head numbers
+__parse (tok:tokens) operations brackets numbers =
     case _type tok of
-        LPAREN -> 
-            let function = head tokens in
-            let number = head (tail tokens) in
-            "    mov rax, " ++ literal number ++ "\n" ++ __parse (tail (tail tokens)) (_type function : LPAREN : operations)
+        LPAREN ->
+            let op = head tokens in
+            let before = peek operations in
+            case before of
+                Nothing ->
+                    __parse (tail tokens) ((_type op, 0) : operations) (LPAREN : brackets) numbers
+                Just x ->
+                    __parse (tail tokens) ((_type op, 0) : (fst x, snd x + 1) : tail operations) (LPAREN : brackets) numbers
         RPAREN ->
-            assert (not (null operations))
-            __parse tokens (tail (tail operations))
+            -- assert (not (null operations))
+            -- assert (head brackets == LPAREN)
+            let (op, n) = head operations in
+            let num = case op of
+                        PLUS -> foldr (+) 0 (reverse (take n numbers))
+                        MINUS -> foldr (-) (head numbers) (reverse (take (n - 1) (tail numbers)))
+                        TIMES -> foldr (*) 1 (reverse (take n numbers))
+                        -- DIVIDE -> foldr (/) (head numbers) (reverse (take (n - 1) (tail numbers)))
+                        _ -> 0 in
+            __parse tokens (tail operations) (tail brackets) (num : drop n numbers)
         NUMBER ->
-            let op = peek operations in
-            case op of
-                -- Just MINUS ->
-                --     "    ; negate number -- NOT IMPEMENTED YET\n" ++ __parse (tok : tokens) (tail operations)
-                Just PLUS ->
-                    "    add rax, " ++ literal tok ++ "\n" ++ __parse tokens operations
-                Just TIMES ->
-                    "    mul rax, " ++ literal tok ++ "\n" ++ __parse tokens operations
-                _ ->
-                    "    ; UNKNOWN OPERATION\n" ++ __parse tokens operations
-        EOF -> assert (null operations) ""
-        _ -> "    ; UNKOWN TOKEN\n" ++ __parse tokens operations
+            let (op, n) = head operations in
+            let _operations = tail operations in
+            __parse tokens ((op, n + 1) : _operations) brackets ((read (literal tok) :: Int) : numbers)
+        EOF -> __parse tokens operations brackets numbers
+        _ -> 0
