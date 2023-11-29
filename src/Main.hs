f281870ad09b6d6cb303a68dac747b1441a4497d
youkwhd <lolywk@tutanota.com>
2023-11-29 18:54:46 +0700

    fix: proper recursion

--- src/Main.hs
+++ src/Main.hs
@@ -6,5 +6,8 @@ import Control.Monad (forM)
 
 main :: IO ()
 main = do
-    let prog = "(* 5 (+ 10 10))"
-    print (parse (tokenize prog))
+    let prog = "(- 10 20 (* 2 2 5))"
+    let r = __parse (tokenize prog)
+    putStr "RESULT: "
+    print (fst r)
+    mapM_ (\tok -> do putStrLn ""; print (_type tok); print (literal tok)) (snd r)
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,40 +1,44 @@
 module Parser where
 
 import Lexer
+import Data.Either
 import Control.Exception
 
 parse :: [Token] -> Int
-parse tokens = fst (__parse tokens PLUS)
+parse tokens = fst (__parse tokens)
 
 peek :: [a] -> Maybe a
 peek [] = Nothing
 peek (x:xs) = Just x
 
-__parse :: [Token] -> TokenType -> (Int, [Token])
-__parse [] _ = (0, [])
-__parse (tok:tokens) op =
+__parseEval :: [Token] -> TokenType -> (Int, [Token])
+__parseEval (tok:tokens) op =
     case _type tok of
+        LPAREN ->
+            let r = __parse (tok : tokens) in
+            let _r = __parseEval (snd r) op in
+            case op of
+                PLUS -> (fst _r + fst r, snd _r)
+                MINUS -> (fst _r - fst r, snd _r)
+                TIMES -> (fst _r * fst r, snd _r)
         RPAREN ->
             (0, [])
-        LPAREN ->
-            __parse tokens op
-        NUMBER ->
-            let next_token = head tokens in
+        _ ->
+            let next  = head tokens in
+            case _type next of
+                RPAREN ->
+                    let val = read (literal tok) :: Int in
+                    (val, tail tokens)
+                _ ->
+                    let r = __parseEval tokens op in
+                    let val = read (literal tok) :: Int in
+                    case op of
+                        PLUS -> (val + fst r, snd r)
+                        MINUS -> (val - fst r, snd r)
+                        TIMES -> (val * fst r, snd r)
 
-            if _type next_token == RPAREN then
-                (read (literal tok) :: Int, tokens)
-            else
-                let r = __parse tokens op in
-                case op of
-                    PLUS ->
-                        ((read (literal tok) :: Int) + fst r, snd r)
-                    MINUS ->
-                        ((read (literal tok) :: Int) - fst r, snd r)
-                    TIMES ->
-                        ((read (literal tok) :: Int) * fst r, snd r)
-        PLUS ->
-            __parse tokens PLUS
-        MINUS ->
-            __parse tokens MINUS
-        TIMES ->
-            __parse tokens TIMES
+
+__parse :: [Token] -> (Int, [Token])
+__parse (tok:tokens) =
+    let op = head tokens in
+    __parseEval (tail tokens) (_type op)
