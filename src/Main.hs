d13a01b74b7ec62466e131a2d012b8d0f41ce40d
youkwhd <lolywk@tutanota.com>
2023-11-29 23:30:20 +0700

    feat: check for number negation

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,6 @@ import Control.Monad (forM)
 
 main :: IO ()
 main = do
-    let prog = "(+ 10 10 (* 20 10) 10)"
-    let r = __parse (tokenize prog)
-    putStr "RESULT: "
-    print (fst r)
-    mapM_ (\tok -> do putStrLn ""; print (_type tok); print (literal tok)) (snd r)
+    let prog = "(+ 1 1 (- 1))"
+    let (result, tokens) = __parse (tokenize prog)
+    print result
--- src/Parser.hs
+++ src/Parser.hs
@@ -12,34 +12,34 @@ peek :: [a] -> Maybe a
 peek [] = Nothing
 peek (x:xs) = Just x
 
-_sum :: [Int] -> Int
-_sum [] = 0
-_sum (x:xs) = x + _sum xs
-
-__parseEval :: [Token] -> TokenType -> Int -> (Int, [Token])
-__parseEval (tok:tokens) op acc =
+__parseEval :: [Token] -> TokenType -> Int -> Int -> (Int, [Token])
+__parseEval [] op iteration acc = (acc, [])
+__parseEval (tok:tokens) op iteration acc =
     case _type tok of
-        LPAREN ->
-            let r = __parse (tok : tokens) in
-            trace ("ACC: -> " ++ show acc) $
-            trace ("R: -> " ++ show (fst r)) $
-            trace ("T: -> " ++ show (snd r)) $
-            case op of
-                PLUS -> __parseEval (snd r) op (acc + fst r)
-                MINUS -> __parseEval (snd r) op (acc - fst r)
-                TIMES -> __parseEval (snd r) op (acc * fst r)
         NUMBER ->
-            let next  = head tokens in
             let val = read (literal tok) :: Int in
             case op of
-                PLUS -> __parseEval tokens op (acc + val)
-                MINUS -> __parseEval tokens op (acc - val)
-                TIMES -> __parseEval tokens op (acc * val)
-        _ ->
-            (acc, tokens)
+                PLUS -> __parseEval tokens op (iteration + 1) (acc + val)
+                MINUS -> __parseEval tokens op (iteration + 1) (acc - val)
+                TIMES -> __parseEval tokens op (iteration + 1) (acc * val)
+        LPAREN ->
+            let result = __parse (tok : tokens) in
+            case op of
+                PLUS -> __parseEval (snd result) op (iteration + 1) (acc + fst result)
+                MINUS -> __parseEval (snd result) op (iteration + 1) (acc - fst result)
+                TIMES -> __parseEval (snd result) op (iteration + 1) (acc * fst result)
+        RPAREN ->
+            let isNegation = iteration == 0 && op == MINUS in
+            (if isNegation then -acc else acc, tokens)
+        _ -> __parseEval tokens op iteration acc
 
 __parse :: [Token] -> (Int, [Token])
 __parse (tok:tokens) =
     let op = head tokens in
-    let firstNum = (read (literal (head (tail tokens))) :: Int) in
-    __parseEval (tail (tail tokens)) (_type op) firstNum
+    case _type (head (tail tokens)) of
+        NUMBER ->
+            let firstNum = (read (literal (head (tail tokens))) :: Int) in
+            __parseEval (tail (tail tokens)) (_type op) 0 firstNum
+        LPAREN ->
+            let result = __parse (tail tokens) in
+            __parseEval (snd result) (_type op) 0 (fst result)
