5eba5e54ab4cfb3a9e8564d8767e76655dbb0118
youkwhd <lolywk@tutanota.com>
2024-04-05 00:25:41 +0700

    feat: add divide functionality

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,11 @@ import Parser
 
 import Control.Monad (forM, when)
 import System.Environment (getArgs, getProgName)
+import Text.Printf
+
+-- Check if double is a fixed number
+isFixed :: Double -> Bool
+isFixed x = x == fromIntegral (truncate x)
 
 printHelp :: String -> IO ()
 printHelp progname = do
@@ -19,7 +24,11 @@ main = do
         [] ->
             printHelp progname
         ["-e", expr] ->
-            print (parse (tokenize expr)) 
+            let result = parse (tokenize expr) in
+            if isFixed result then
+                print (fromIntegral (truncate result))
+            else
+                print result
         [file] ->
             -- TODO:
             -- read from file
--- src/Parser.hs
+++ src/Parser.hs
@@ -4,25 +4,25 @@ import Lexer
 import Data.Either
 import Control.Exception
 
-parse :: [Token] -> Int
+parse :: [Token] -> Double
 parse tokens = fst (parseHead tokens)
 
-parseHead :: [Token] -> (Int, [Token])
+parseHead :: [Token] -> (Double, [Token])
 parseHead (tok:tokens) 
     | _type tok /= LPAREN = error "`tok` should be LPAREN"
     | otherwise =
         let op = head tokens in
         parseBodyFirstNum (tail tokens) (_type op)
 
-parseBodyFirstNum :: [Token] -> TokenType -> (Int, [Token])
+parseBodyFirstNum :: [Token] -> TokenType -> (Double, [Token])
 parseBodyFirstNum (tok:tokens) op 
     | _type tok == LPAREN = 
         let (result, rest) = parseHead (tok : tokens) in
         parseBody rest op 0 result
-    | _type tok == NUMBER = parseBody tokens op 1 (read (literal tok) :: Int) 
+    | _type tok == NUMBER = parseBody tokens op 1 (read (literal tok) :: Double)
     | otherwise = error "unknown pattern"
 
-parseBody :: [Token] -> TokenType -> Int -> Int -> (Int, [Token])
+parseBody :: [Token] -> TokenType -> Int -> Double -> (Double, [Token])
 parseBody [] op iteration acc = (acc, [])
 parseBody (tok:tokens) op iteration acc =
     case _type tok of
@@ -32,12 +32,14 @@ parseBody (tok:tokens) op iteration acc =
                 PLUS -> parseBody rest op (iteration + 1) (acc + val)
                 MINUS -> parseBody rest op (iteration + 1) (acc - val)
                 TIMES -> parseBody rest op (iteration + 1) (acc * val)
+                DIVIDE -> parseBody rest op (iteration + 1) (acc / val)
         NUMBER ->
-            let val = read (literal tok) :: Int in
+            let val = read (literal tok) :: Double in
             case op of
                 PLUS -> parseBody tokens op (iteration + 1) (acc + val)
                 MINUS -> parseBody tokens op (iteration + 1) (acc - val)
                 TIMES -> parseBody tokens op (iteration + 1) (acc * val)
+                DIVIDE -> parseBody tokens op (iteration + 1) (acc / val)
         RPAREN ->
             let isNegation = iteration == 0 && op == MINUS in
             (if isNegation then -acc else acc, tokens)
